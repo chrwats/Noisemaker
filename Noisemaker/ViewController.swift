025fb4be3268c17e5d40cdb3e56950af2943adf3
chrwats <chrwats@users.noreply.github.com>
2017-05-21 19:13:45 -0500

    Initial View - 4 sounds/ icons

--- Noisemaker/ViewController.swift
+++ Noisemaker/ViewController.swift
@@ -7,19 +7,30 @@
 //
 
 import UIKit
+import AVFoundation
+
+var player: AVAudioPlayer?
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    @IBAction func playSound(_ sender: UIButton) {
+        if let buttonName = sender.currentTitle {
+            playAudio(buttonName: buttonName)
+        }
+    }
+    
+    func playAudio(buttonName: String) {
+        if let url = Bundle.main.url(forResource: buttonName, withExtension: "wav") {player = try? AVAudioPlayer(contentsOf: url)
+            player?.play()}
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    
 }
 
